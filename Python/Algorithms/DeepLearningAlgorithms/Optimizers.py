61c5f600411a5f3de5a9e016a53b68c1e207f630
Malav Patel <144440737+Malav5372@users.noreply.github.com>
2023-12-25 14:03:48 +0530

    Update Optimizers.py

--- Python/Algorithms/DeepLearningAlgorithms/Optimizers.py
+++ Python/Algorithms/DeepLearningAlgorithms/Optimizers.py
@@ -1,9 +1,4 @@
-"""
-__FileCreationDate__  :  //2020
-__Author__           :  CodePerfectPlus
-__Package__         :  Python 3
-__GitHub__         : https://www.github.com/codeperfectplus
-"""
+
 import numpy as np
 
 
